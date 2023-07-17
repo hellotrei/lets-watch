e31344d909d4e3a452a7ce749503a0fc6acf59ca
Treido <treido.vendor.pasti@pegadaian.co.id>
2023-07-17 14:37:53 +0700

    update spinner styles

--- src/modules/shared/components/Spinner/Spinner.tsx
+++ src/modules/shared/components/Spinner/Spinner.tsx
@@ -3,7 +3,9 @@ import './Spinner.css'; // Import the CSS file for styling
 
 const Spinner: React.FC = () => {
   return (
+    <div className="spinner-container">
       <div className="spinner"></div>
+    </div>
   );
 };
 
